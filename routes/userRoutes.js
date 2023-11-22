c0af5a2c09dd19f42cfdc1514dbd75684842b907
Chitransh Sharma <mylife.myrule0321@gmail.com>
2023-11-23 01:07:00 +0530

    revert changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,10 @@ const {
   allUsers,
   registerUser,
   authUser,
+  addToCart,
+  addToWishlist,
+  getCartItems,
+  getWishItems,
 } = require("../controllers/userControllers");
 
 const router = express.Router();
@@ -12,5 +16,9 @@ const router = express.Router();
 router.route("/").get(protect, allUsers);
 router.route("/").post(registerUser);
 router.post("/login", authUser);
+router.post("/addToCart/:id", protect, addToCart);
+router.patch("/addToWishList/:id", protect, addToWishlist);
+router.get("/getCart", protect, getCartItems);
+router.get("/getWishList", protect, getWishItems);
 
 module.exports = router;
